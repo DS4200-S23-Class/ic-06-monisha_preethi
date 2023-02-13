61525d21506ecc6cad35c4b3e748cc86228891d2
Monisha Kapadia <kapadia.mo@northeastern.edu>
2023-02-13 15:19:09 -0500

    done

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+function ButtonClicked() {
+	let line2= document.getElementById("line");
+	line2.classList.add("red");
+	console.log("hi")
+
+
+};
+
+document.getElementById("button1").addEventListener("click", ButtonClicked);
